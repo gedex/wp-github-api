5405b8a7100603343de625cdad3eb83b81206554
Akeda Bagus <akeda@x-team.com>
2014-04-21 01:59:13 +0700

    Fix typo in css filename.

--- includes/script-loader.php
+++ includes/script-loader.php
@@ -19,6 +19,6 @@ class WP_GitHub_API_Script_Loader {
 
 		wp_register_script( 'gh-d3', $plugin->js_url . 'd3.min.js', array(), $plugin->version );
 
-		wp_register_style( 'gh-octicons', $plugin->css_url . 'octions.min.css', array(), $plugin->version, 'all' );
+		wp_register_style( 'gh-octicons', $plugin->css_url . 'octicons.min.css', array(), $plugin->version, 'all' );
 	}
 }
